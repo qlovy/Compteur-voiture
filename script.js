3a10dfd68fde48e1803cd69c608490389cf2d29b
qlovy <qlovy@sysnet.ch>
2023-10-26 14:52:07 +0200

    mise en place de la pédale et du cadran de vitesse.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /*DECLARATION VARIABLE*/
 
 //Le canvas de l'accélérateur
-const canvasAccelerator = document.getElementById("accélérateur");
+const canvasAccelerator = document.querySelector("#accélérateur");
 const ctxA = canvasAccelerator.getContext('2d');
 
-//Le canvas du  cadran de vitesse
+//Le canvas du cadran de vitesse
 const canvasSpeedCounter = document.getElementById("cadranVitesse");
 const ctxS = canvasSpeedCounter.getContext('2d');
 
@@ -14,11 +14,18 @@ const ctxS = canvasSpeedCounter.getContext('2d');
 function Accelerator (config) {
     this.x = config.x || 0;
     this.y = config.y || 0;
-    this.width = config.width || 130;
+    this.width = config.width || 150;
+    this.height = config.height || 380;
     this.ironColor = config.color || 'rgb(149, 165, 166)';
     this.gripColor = config.color || 'rgb(44, 62, 80)';
 }
 
 Accelerator.prototype.drawRelease = function () {
+    ctxA.rect(this.x, this.y, this.width,  this.height);
+}
+
+/*APPEL DES FONCTIONS*/
+let accelerator = {
 
 }
+accelerator.drawRelease();
