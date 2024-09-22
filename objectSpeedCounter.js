7be1338cf61b74134be4749b3e3e329cf9a9948d
qlovy <qlovy@sysnet.ch>
2024-09-22 20:53:23 +0200

    boucle quand la souris est pressée + esthétique.

--- objectSpeedCounter.js
+++ objectSpeedCounter.js
@@ -14,10 +14,12 @@ function SpeedCounter(config) {
     this.angleGraduation = config.angleGraduation || 180 / this.nbGraduation;
     this.pointerColor = config.pointerColor || 'rgb(255, 0, 0)';
     this.pointerRadius = config.pointerRadius || 20;
-    this.i = config.i || 1;
+    this.i = config.i || 0;
 }
 
 SpeedCounter.prototype.drawBackground = function () {
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     // Fond du cadran
     this.ctx.fillStyle = this.backgroundColor;
     // le haut du cadran
@@ -25,30 +27,38 @@ SpeedCounter.prototype.drawBackground = function () {
     this.ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.radius, degToRad(180), degToRad(360));
     this.ctx.fill();
     // le bas du cadran
-    this.ctx.fillRect(this.x + this.width/2 - this.radius, this.y + this.height/2, this.radius*2, 30);
+    this.ctx.fillRect(this.x + this.width / 2 - this.radius, this.y + this.height / 2, this.radius * 2, 30);
 
     // la graduation
     this.ctx.fillStyle = this.graduationColor;
-    for (let i=0; i<10; i++){
+    for (let i = 0; i < 10; i++) {
         this.ctx.save();
         // Math.cos et Math.sin permet de calculer la distance en x et respectivement y pour le suivit du cercle.
-        this.ctx.translate(this.width/2 + Math.cos(Math.PI/180 * this.angleGraduation * i + Math.PI) * (this.radius - 40), this.height/2 + Math.sin(Math.PI/180 * this.angleGraduation * i + Math.PI) * (this.radius - 40));
-        this.ctx.rotate(Math.PI/180 * this.angleGraduation * i + Math.PI);
+        this.ctx.translate(this.width / 2 + Math.cos(Math.PI / 180 * this.angleGraduation * i + Math.PI) * (this.radius - 40), this.height / 2 + Math.sin(Math.PI / 180 * this.angleGraduation * i + Math.PI) * (this.radius - 40));
+        this.ctx.rotate(Math.PI / 180 * this.angleGraduation * i + Math.PI);
         this.ctx.fillRect(0, -2, 15, 5);
         this.ctx.restore();
     }
 }
 
-SpeedCounter.prototype.drawRelease = function(){
+SpeedCounter.prototype.drawRelease = function () {
+    this.i = 0;
     this.drawBackground();
     // l'aiguille
     // le cercle de base
     this.ctx.fillStyle = this.pointerColor;
     this.ctx.beginPath();
-    this.ctx.arc(this.x + this.width/2, this.y + this.height/2, this.pointerRadius, degToRad(0), degToRad(360));
+    this.ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.pointerRadius, degToRad(0), degToRad(360));
     this.ctx.fill();
 
     // la pointe
+    this.ctx.save();
+    this.ctx.fillStyle = this.pointerColor;
+    this.ctx.translate(this.width / 2, this.height / 2);
+    this.ctx.fillRect(0, 0, -100, 2);
+    this.ctx.restore();
+
+    /*
     this.ctx.save();
     this.ctx.fillStyle = this.pointerColor;
     this.ctx.translate(this.width/2, this.height/2 + this.pointerRadius);// Place le système d'axe au en bas du cercle de base
@@ -58,13 +68,13 @@ SpeedCounter.prototype.drawRelease = function(){
     this.ctx.lineTo(0, 0);// On complète la forme
     this.ctx.fill();
     this.ctx.restore();
+    */
 
     // l'axe de rotation
     this.ctx.fillStyle = 'rgb(255, 255, 255)';
     this.ctx.beginPath();
-    this.ctx.arc(this.x + this.width/2, this.y + this.height/2, 6, degToRad(0), degToRad(360));
+    this.ctx.arc(this.x + this.width / 2, this.y + this.height / 2, 6, degToRad(0), degToRad(360));
     this.ctx.fill();
-    this.i = 0;
 }
 
 SpeedCounter.prototype.drawAcceleration = function () {
@@ -73,26 +83,28 @@ SpeedCounter.prototype.drawAcceleration = function () {
     // le cercle de base
     this.ctx.fillStyle = this.pointerColor;
     this.ctx.beginPath();
-    this.ctx.arc(this.x + this.width/2, this.y + this.height/2, this.pointerRadius, degToRad(0), degToRad(360));
+    this.ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.pointerRadius, degToRad(0), degToRad(360));
     this.ctx.fill();
 
     // la pointe
-    let angle = this.i;
+    let angle = degToRad(this.i + 180);
+    let dx = Math.cos(angle) * 100;
+    let dy = Math.sin(angle) * 100;
     this.ctx.save();
     this.ctx.fillStyle = this.pointerColor;
-    this.ctx.translate(this.width/2 - this.pointerRadius * Math.sin(Math.PI/180 * angle), this.height/2 + this.pointerRadius - 1);// Place le système d'axe au en bas du cercle de base
-    this.ctx.rotate(Math.PI/180 * angle);
+    this.ctx.translate(this.width / 2, this.height / 2);// Place le système d'axe au centre du cercle de base
     this.ctx.beginPath();
-    this.ctx.lineTo(-this.radius + 60, -this.pointerRadius);// La pointe
-    this.ctx.lineTo(0, -2 * this.pointerRadius);// Le point le plus haut du cercle de base
-    this.ctx.lineTo(0, 0);// On complète la forme
+    this.ctx.lineTo(dx, dy);
+    this.ctx.lineTo(dx + 1, dy + 1);
+    this.ctx.lineTo(1, 1);
+    this.ctx.lineTo(0, 0);
     this.ctx.fill();
     this.ctx.restore();
 
     // l'axe de rotation
     this.ctx.fillStyle = 'rgb(255, 255, 255)';
     this.ctx.beginPath();
-    this.ctx.arc(this.x + this.width/2, this.y + this.height/2, 6, degToRad(0), degToRad(360));
+    this.ctx.arc(this.x + this.width / 2, this.y + this.height / 2, 6, degToRad(0), degToRad(360));
     this.ctx.fill();
-    this.i++;
+    this.i = this.i + 0.1;
 }
--- script.js
+++ script.js
@@ -20,22 +20,33 @@ let accelerator = new Accelerator({});
 let speedCoutner = new SpeedCounter({});
 
 // Au chargement de la page, on dessine les éléments en position initiale
-window.onload = function () { 
+window.onload = function () {
     accelerator.drawRelease();
     speedCoutner.drawRelease();
 }
 
-function draw(){
+function draw() {
+    // contient le numéro de processus
+    let id;
     // Quand on appuie sur la pédale d'accélérateur
     accelerator.canvas.addEventListener('mousedown', () => {
-        accelerator.drawPressed();          // Dessin de la pédale appuyée
-        speedCoutner.drawAcceleration();    // Dessin de l'aiguille qui tourne
+        // Défini une boucle sous form d'intervalle régulière de 100 ms
+        id = setInterval(
+            () => {
+                accelerator.drawPressed();          // Dessin de la pédale appuyée
+                speedCoutner.drawAcceleration();    // Dessin de l'aiguille qui tourne
+            },
+            100
+        )
     })
 
     accelerator.canvas.addEventListener('mouseup', () => {
         accelerator.drawRelease();
         speedCoutner.drawRelease();
+        // Arrête la boucle
+        clearInterval(id);
     })
-    window.requestAnimationFrame(draw);//exécute la fonction draw 60 fois par secondes.
+    window.requestAnimationFrame(draw);//exécute la fonction draw 60 fois par secondes
 }
+
 draw();
